73c78ed456a53bbf709ce3c874b586fde10fe6d8
Capitos Kamal <36074920+kamalkarlos@users.noreply.github.com>
2022-02-21 00:10:25 +0100

    Uploader
    Assistant lifting Scripts such as Shell or index or anything

---
+++ up.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Uploader | Capitos Kamal</title>
+  <link rel="icon" type="image/jpg" href="https://images.vectorhq.com/images/previews/140/the-amazigh-logo-psd-444871.png">
+  <link rel="icon" type="image/x-icon" href="https://images.vectorhq.com/images/previews/140/the-amazigh-logo-psd-444871.png">
+  <meta name="title" content="Shadow Cyber Team">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="description" content="Indonesian People">
+  <meta name="googlebot" content="index,follow">
+  <meta name="robots" content="all">
+  <meta name="robots schedule" content="auto">
+  <meta name="distribution" content="global">
+  <meta property="og:image" content="https://images.vectorhq.com/images/previews/140/the-amazigh-logo-psd-444871.png">
+  <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Aleo:ital@1&display=swap" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&display=swap" rel="stylesheet">
+  <style> body {background: black; color: white;}</style>
+  <center><embed src="https://a.top4top.io/m_2236geby61.mp3" width="none" height="none" loop="true" autostart="true" hidden="true"></embed></center>
+</head>
+</style>
+  <style>
+
+        .tengah{
+
+            position: absolute;margin-top: -50px;margin-left: -200px;left: 50%;top: 50%;width: 400px;height: 220px;background-color: ;
+            
+        }
+        .alfanxploit{
+            background:transparent;
+            border:2px solid red;
+            border-radius: 3px;
+            padding:6px 60px;
+            
+        }
+        .foren{
+            background: transparent;
+            border:4px solid red;
+            border-radius:4px;
+            font-color:white;
+            padding:5px 5px;
+            }
+            
+          input{
+            color:red;
+            }
+</style>
+<body>
+<center><button class="alfanxploit"</center>
+<br>
+<center><p><font face="Aleo" size="3px" color='white'> </> Uploader Capitos Kamal </> </font></p><center><br>
+<form method='post' enctype='multipart/form-data'>
+      <input type='file' name='idx_file'>
+      <input type='submit' name='upload' value='upload'>
+      </form><br>
+      </form>
+    </div>
+  <center><p><font face="Aleo" size="3px" color='white'>Copyright Anonymous Imazighen Morocco</font></p><center>
+</body>
